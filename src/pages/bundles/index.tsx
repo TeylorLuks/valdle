c8af5533d9a89b8db93773bf463a849919d3c08f
Teylor Lucas A. Silva <45246094+TeylorLuks@users.noreply.github.com>
2022-09-05 18:30:55 -0300

    Assets Path
    - Adição de Fontes
    - Adição de cards
    - Ajuste no Botão Principal

--- src/pages/bundles/index.tsx
+++ src/pages/bundles/index.tsx
@@ -10,17 +10,17 @@ export default function Bundles(){
   const router = useRouter();
   const [dados,setDados] = useState(null);
 
-  // useEffect(()=>{
-  //   axios.get('http://valdle-api.herokuapp.com/api/v1/bundle/',{
-  //     headers:{
-  //       'Access-Control-Allow-Origin': '*'
-  //     }
-  //   })
-  //   .then(res => {
-  //     setDados(res.data)
-  //     console.log(res.data)
-  //   })
-  // },[])
+  useEffect(()=>{
+    axios.get('https://api.valdle.com/v1/bundle/random',{
+      headers:{
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+    .then(res => {
+      setDados(res.data)
+      console.log(res.data)
+    })
+  },[])
 
   return(
     <div className={styles.containerGeral}>
@@ -39,7 +39,7 @@ export default function Bundles(){
               </div>
             </div>
           </div> 
-          <div className={styles.containerButtons}>    
+          <div className={styles.containerButtons}> 
             <button
               onClick={()=> alert('FUDEU')}
             >1</button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,34 +16,32 @@ export default function Home() {
     {
       gameName: 'GUESS THE MAP',
       gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',
-      urlCard: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt02c83424f7b41a97/6243813d8850ee0e8ea0ae56/maps-03ebbf2c074f13a65af1dba0c80f767e.png',
+      urlCard: '/cards/mapCard.png',
       page: '/map'
     },
     {
       gameName: 'WORDLE',
-      gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',
-      urlCard: 'https://i.pinimg.com/originals/1f/9d/9c/1f9d9c469dd48b9a9f24e2d6c34a8d5c.jpg',
+      gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',      
     },
     {
       gameName: 'GUESS THE VOICE',
       gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',
-      urlCard: 'https://media.discordapp.net/attachments/933773884640096377/1011079982539345992/909837df-dcd5-4019-bde3-d463c08929cf.jpg',
+      urlCard: '/cards/voiceCard.png',
+      page: '/voices'
     },
     {
       gameName: 'GUESS THE ABILITY',
-      gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',
-      urlCard: 'https://cdna.artstation.com/p/assets/images/images/037/564/824/large/brandon-mcdonald-memedog2-copy2.jpg?1620717308&dl=1',
+      gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',      
     }, 
     {
       gameName: 'GUESS THE BUNDLE',
       gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',
-      urlCard: 'https://rd1.com.br/wp-content/uploads/2020/09/20200915-rd1-programa-raul-gil.png',
+      urlCard: '/cards/weaponCard.png',
       page: '/bundles'
     }, 
     {
       gameName: 'QUIZZ VALORANT',
-      gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',
-      urlCard: 'https://pbs.twimg.com/media/E6Sh8PxXsAYK8Dd.jpg',
+      gameDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, quisquam laborum voluptates laboriosam officia pariatur autem hic accusantium magnam amet illum repellat nam, cumque dolore ducimus omnis nisi. Amet labore doloremque voluptate dignissimos et eaque quia doloribus libero vel quasi iure ab porro pariatur in a nam distinctio, necessitatibus, iusto ut. Repellendus incidunt error voluptas.',      
     }
   ]
 
@@ -98,10 +96,25 @@ export default function Home() {
                 } : {
                   height: '60vh',                                
                 }}
-              >                
-                <img src={item.urlCard} alt="" />
+              >           
                 {
-                  currentSlide === index &&
+                  item.urlCard ?
+                    <img src={item.urlCard} alt="" />
+                  :
+                    <h3
+                      style={{
+                        height: '100%',
+                        textAlign: 'center',
+                        alignItems: 'center',
+                        display: 'grid',
+                        color: 'var(--white)',
+                        fontSize: '3rem'
+                      }}
+                    >LOADING...</h3>
+                }
+                
+                {
+                  (currentSlide === index && item.page) &&
                     <Button
                       text='JOGAR'
                       onClick={() => {
